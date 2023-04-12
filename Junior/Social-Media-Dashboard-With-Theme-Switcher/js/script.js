9e81cd31322f2d6a88c36aec69cb1e89763fc160
Gio Cura <patrickgio523@gmail.com>
2023-04-12 11:51:40 +0800

    Made mobile layout

--- Junior/Social-Media-Dashboard-With-Theme-Switcher/js/script.js
+++ Junior/Social-Media-Dashboard-With-Theme-Switcher/js/script.js
@@ -1,12 +1,20 @@
+"use strict";
+
 const darkmodeInputEl = document.querySelector(".darkmode__input");
 const darkmodeToggleEl = document.querySelector(".darkmode__toggle");
+
+// For transitions that fire prematurely on load
+// Just place onload=noTransition() to element of choice.
+// function noTransition(e) {
+//   e.classList.remove("preload");
+// }
 
 function toggleTheme() {
   document.body.classList.toggle("light");
 }
 
 darkmodeToggleEl.addEventListener("keydown", (e) => {
-  // prevents default spacebar scrolling
+  // Prevents default spacebar scrolling
   e.preventDefault();
   if (e.key === " " || e.key === "Enter") {
     darkmodeInputEl.click();
