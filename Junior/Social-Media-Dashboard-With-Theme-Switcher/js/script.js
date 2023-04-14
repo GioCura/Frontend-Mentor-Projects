33ec063171720dd0c181d0ece34e1fd0ca6d98e6
Gio Cura <patrickgio523@gmail.com>
2023-04-14 14:51:24 +0800

    Made script for removing will-change

--- Junior/Social-Media-Dashboard-With-Theme-Switcher/js/script.js
+++ Junior/Social-Media-Dashboard-With-Theme-Switcher/js/script.js
@@ -2,13 +2,22 @@
 
 const darkmodeInputEl = document.querySelector(".darkmode__input");
 const darkmodeToggleEl = document.querySelector(".darkmode__toggle");
-const cardEl = document.querySelectorAll(".card");
-const cardRateEl = document.querySelectorAll(".card__rate");
+const renderFixEl = document.querySelectorAll(".renderfix");
+
+// Resets the will-change as per best practice
+function renderFix() {
+  setTimeout(function () {
+    renderFixEl.forEach((e) => {
+      e.style.willChange = "auto";
+    });
+  }, 5000);
+}
 
 function toggleTheme() {
   document.body.classList.toggle("light");
 }
 
+// Enables toggle to be activated by spacebar or enter when it's focused
 darkmodeToggleEl.addEventListener("keydown", (e) => {
   // Prevents default spacebar scrolling
   e.preventDefault();
