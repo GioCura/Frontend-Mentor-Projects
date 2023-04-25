71192f5e9901d35ccbd08592d00cf796a53a7309
Gio Cura <patrickgio523@gmail.com>
2023-04-25 18:40:45 +0800

    Added autocomplete script for Firefox

--- Newbie/Ping-Coming-Soon-Page/script.js
+++ Newbie/Ping-Coming-Soon-Page/script.js
@@ -1,8 +1,12 @@
 "use strict";
 
 const form = document.querySelector(".hero__cta");
+const ctaEmail = document.querySelector(".cta__email");
 const email = document.getElementById("email");
 const emailErrorEl = document.querySelector(".email__error");
+
+// REENABLES AUTOCOMPLETE (FOR FIREFOX)
+ctaEmail.autocomplete = "on";
 
 // VALIDATES THE FORM BEFORE AND AFTER SUBMITTING //
 email.addEventListener("change", () => {
