31bcc6cb086722827b53a3d134a784da32f75b67
Gio Cura <patrickgio523@gmail.com>
2023-04-25 18:14:47 +0800

    Styled range slider for webkit and moz

--- Junior/Interactive-Pricing-Component/js/script.js
+++ Junior/Interactive-Pricing-Component/js/script.js
@@ -10,7 +10,22 @@ sliderMeasure.textContent = sliderInput.value;
 
 sliderInput.addEventListener("input", (event) => {
   sliderMeasure.textContent = event.target.value;
-  let e = Number(sliderMeasure.textContent);
+
+  // Dynamically sets progress bar
+  let progressValue =
+    ((event.target.value - event.target.min) /
+      (event.target.max - event.target.min)) *
+    100;
+
+  event.target.style.background =
+    "linear-gradient(to right, #A4F3EB 0%, #A4F3EB " +
+    progressValue +
+    "%, #ecf0fb " +
+    progressValue +
+    "%, #ecf0fb 100%)";
+
+  // Switches between categories
+  let e = Number(event.target.value);
 
   if (e === 5) {
     pricingCategory.className = "pricing__category category--1m";
