7eca944fd6659ee09b6a7496a68080b6e35be656
Gio Cura <patrickgio523@gmail.com>
2023-06-12 14:27:51 +0800

    Added animation and noscript fallback

--- Newbie/Workit-Landing-Page/js/script.js
+++ Newbie/Workit-Landing-Page/js/script.js
@@ -1,1 +1,28 @@
 "use strict";
+
+const homeTitle = document.querySelector(".home h1");
+const greenLinkHome = document.querySelector(".green-link--home");
+const loadAnim = document.querySelectorAll(".load-anim");
+
+let options = {
+  threshold: 0.6,
+};
+
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.classList.remove("fade-in");
+      entry.target.classList.remove("fade-left");
+      entry.target.classList.remove("fade-right");
+    }
+  });
+}, options);
+
+loadAnim.forEach((el) => {
+  observer.observe(el);
+});
+
+window.addEventListener("load", function () {
+  homeTitle.classList.remove("fade-in");
+  greenLinkHome.classList.remove("fade-in");
+});
