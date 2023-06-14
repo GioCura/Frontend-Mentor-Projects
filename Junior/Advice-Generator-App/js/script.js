faf56e3944c37c8653f34561a18ecec05924a406
Gio Cura <patrickgio523@gmail.com>
2023-06-14 12:58:47 +0800

    Added loading script

--- Junior/Advice-Generator-App/js/script.js
+++ Junior/Advice-Generator-App/js/script.js
@@ -1,5 +1,6 @@
 "use strict";
 
+const adviceTitle = document.querySelector(".advice-generator h1");
 const adviceId = document.querySelector(".advice__id");
 const advice = document.querySelector(".advice");
 const adviceBtn = document.querySelector(".advice__btn");
@@ -7,14 +8,17 @@ const adviceBtn = document.querySelector(".advice__btn");
 adviceBtn.addEventListener("click", getAdvice);
 
 function getAdvice() {
+  adviceTitle.style.display = "none";
+  advice.innerHTML = `<span class="loading"></span>`;
   fetch("https://api.adviceslip.com/advice")
     .then((response) => {
       return response.json();
     })
     .then((adviceData) => {
       const adviceObj = adviceData.slip;
+      adviceTitle.style.display = "block";
       adviceId.textContent = adviceObj.id;
-      advice.textContent = adviceObj.advice;
+      advice.innerHTML = `“${adviceObj.advice}”`;
     })
     .catch((error) => {
       console.log(error);
